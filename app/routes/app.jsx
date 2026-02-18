2fcb4558d89a0a7830d43facacd154f74ead9267
rina-sogody <rina.rexhepi@sogody.com>
2026-02-18 16:57:20 +0100

    fix

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -18,7 +18,7 @@ export default function App() {
       <s-app-nav>
         <s-link href="/app/free-gift">Free Gift</s-link>
         <s-link href="/app/free-gift-by-variant">Free Gift By Variant</s-link>
-        <s-link href="/app/flex-discount">Flex Discount</s-link>
+        <s-link href="/app/flex-discount">Tiered Discount</s-link>
       </s-app-nav>
       <Outlet />
     </AppProvider>
